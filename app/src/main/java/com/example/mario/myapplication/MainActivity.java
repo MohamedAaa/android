2b52487cb9e0de2cbe19d3254c470d497c685fe5
MohamedAaa <m.3laa.95@gmail.com>
2017-08-23 22:30:38 +0200

    third

--- app/src/main/java/com/example/mario/myapplication/MainActivity.java
+++ app/src/main/java/com/example/mario/myapplication/MainActivity.java
@@ -5,16 +5,29 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
-
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    Button btn1,btn2;
     private static int i =0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Toast.makeText(this, "OnCreate", Toast.LENGTH_SHORT).show();
+        btn1 = (Button) findViewById(R.id.button1);
+        btn1.setOnClickListener(this);
+
+        btn2 = (Button) findViewById(R.id.reset);
+        // thierd method useing inner calss variable  .
+        btn2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                i = 1;
+                Toast.makeText(MainActivity.this, "I =" +i, Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 
     @Override
@@ -62,14 +75,22 @@ public class MainActivity extends AppCompatActivity {
         super.onConfigurationChanged(newConfig);
         Log.d("MSG","orientaion Changes"+Configuration.ORIENTATION_PORTRAIT);
     }
+    // second method useing impelments onclicklestener inteface
+    @Override
+    public void onClick(View view) {
+            Toast.makeText(this, "I = "+(++i), Toast.LENGTH_SHORT).show();
 
+    }
+    /* first method useing onclick in xml file
     public void addNum(View view) {
-        if (view.getId() == R.id.button){
+        if (view.getId() == R.id.button1){
             Toast.makeText(this, "I = "+(++i), Toast.LENGTH_SHORT).show();
         }else{
             i = 0;
             Toast.makeText(this, "I = "+(++i), Toast.LENGTH_SHORT).show();
-
         }
     }
+
+    */
+
 }
