742dfd98bb4d3987a04d92f482ba45ef06b59c54
MohamedAaa <m.3laa.95@gmail.com>
2017-08-24 05:58:58 +0200

    intent activity

--- app/src/main/java/com/example/mario/myapplication/MainActivity.java
+++ app/src/main/java/com/example/mario/myapplication/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.mario.myapplication;
 
+import android.content.Intent;
 import android.content.res.Configuration;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -9,7 +10,7 @@ import android.widget.Button;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
-    Button btn1,btn2;
+    Button btn1,btn2,second;
     private static int i =0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,7 +21,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         btn1.setOnClickListener(this);
 
         btn2 = (Button) findViewById(R.id.reset);
-        // thierd method useing inner calss variable  .
+        // third method useing inner calss variable  .
         btn2.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -28,47 +29,26 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 Toast.makeText(MainActivity.this, "I =" +i, Toast.LENGTH_SHORT).show();
             }
         });
+
+        second  = (Button) findViewById(R.id.second);
+        second.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(MainActivity.this,second.class);
+                startActivity(i);
+            }
+        });
     }
 
-    @Override
-    protected void onRestart() {
-        super.onRestart();
-        Toast.makeText(this,"OnRestart",Toast.LENGTH_SHORT).show();
-    }
-
-    @Override
-    protected void onStart() {
-        super.onStart();
-        Toast.makeText(this,"OnStart",Toast.LENGTH_SHORT).show();
-
-    }
 
     @Override
     protected void onResume() {
         super.onResume();
-        Toast.makeText(this,"OnRsume",Toast.LENGTH_SHORT).show();
 
     }
 
-    @Override
-    protected void onPause() {
-        super.onPause();
-        Toast.makeText(this,"onPause",Toast.LENGTH_SHORT).show();
 
-    }
 
-    @Override
-    protected void onStop() {
-        super.onStop();
-        Toast.makeText(this,"onStop",Toast.LENGTH_SHORT).show();
-    }
-
-    @Override
-    protected void onDestroy() {
-        super.onDestroy();
-        Toast.makeText(this,"onDestroy",Toast.LENGTH_SHORT).show();
-
-    }
 
     @Override
     public void onConfigurationChanged(Configuration newConfig) {
